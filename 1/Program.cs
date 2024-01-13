2cf2c5790e592b07e6f908e298a6e8040578e8d4
Levente Botos <botos.levente2007@gmail.com>
2024-01-13 13:35:29 +0100

    done

---
+++ 1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+            string input1 = Console.ReadLine();
+            string[] split1 = input1.Split(' ');
+            int retek = int.Parse(split1[0]);
+            int minszam = int.Parse(split1[1]);
+           
+            string input2 = Console.ReadLine();
+            string[] split2 = input2.Split(' ');
+
+            int res = 0;
+            List<int> szabade = new List<int>();
+            foreach (string szam in split2)
+            {
+                szabade.Add(int.Parse(szam));
+            }
+           
+
+
+
+            for (int i = 0; i < retek; i++)
+            {
+                int tempres = 0;
+                if (szabade[i] == 1)
+                {
+                    for (int x = i; x < retek; x++)
+                    {
+
+                        if (szabade[x] == 1)
+                        {
+                            tempres += 1;
+                        }
+                    }
+                    if (tempres > minszam || tempres == minszam)
+                    {
+                        tempres = tempres - 2;
+                    }
+                    else
+                    {
+                        tempres = 0;
+                    }
+                    // Console.WriteLine(tempres);
+                }
+                res += tempres;
+            }
+           
+            Console.WriteLine(res);
+           
+        }
+
+
+    }
+}
---
+++ 3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int res = 0;
+
+            string input1 = Console.ReadLine();
+            string[] split1 = input1.Split(' ');
+            int sorok = int.Parse(split1[0]);
+            int oszlopok = int.Parse(split1[1]);
+
+            string input2 = Console.ReadLine();
+            string[] split2 = input2.Split(' ');
+            int egeszElsoSor = int.Parse(split2[0]);
+            int egeszMasodikSor = int.Parse(split2[1]);
+
+            List<int[]> oszlopokValues = new List<int[]>();
+            for (int i = 0; i < oszlopok; i++)
+            {
+                string newInput = Console.ReadLine();
+                string[] newSplit = newInput.Split(' ');
+                List<int> temp = new List<int>();
+                foreach (string item in newSplit)
+                {
+                    temp.Add(int.Parse(item));
+                }
+
+                int[] temp2 = temp.ToArray();
+                oszlopokValues.Add(temp2);
+
+            }
+            int Sum(int inp)
+            {
+                int res = 0;
+                
+                    foreach (int[] item in oszlopokValues)
+                    {
+                        if (item[inp] > item[0]){
+                        res += item[0];
+                        }
+                        else {
+                             res += item[inp];
+                        }
+                    }
+               
+              
+               
+                return res;
+            }
+
+
+        for (int i = 0; i > oszlopok; 1++) {
+            
+        }
+            if (egeszElsoSor < Sum(0))
+            {
+                Console.WriteLine(true);
+                res += egeszElsoSor;
+            }
+            else
+            {
+                Console.WriteLine(false);
+                res += Sum(0);
+
+            }
+
+            if (egeszMasodikSor < Sum(2))
+            {
+                Console.WriteLine(true);
+                res += egeszMasodikSor;
+            }
+            else
+            {
+                Console.WriteLine(false);
+                res += Sum(2);
+
+            }
+            Console.WriteLine(res);
+        }
+
+
+
+    }
+}
